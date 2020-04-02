7c6a1f910f49d8489d88ca8f1ef67ad89eff9161
sayantan01 <cavi4762@gmail.com>
2020-04-02 20:14:15 +0530

    express prototype created

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const app=express();
+const path=require('path');
+
+let PORT=process.env.PORT||8080
+
+app.use(express.static("public"));
+
+app.all('*',(req,res)=>{
+    res.send("<h1>404 Not Found</h1>");
+});
+
+app.listen(PORT,()=>console.log('App listening on port 8080'));
